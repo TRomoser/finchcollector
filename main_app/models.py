57c78d386f73c2ee6a74eb46b642515b3ca9e920
tromoser <tromoser@gmail.com>
2022-11-17 10:03:22 -0800

    part 4

--- main_app/models.py
+++ main_app/models.py
@@ -34,7 +34,7 @@ class Feeding(models.Model):
 
   finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
 
-  def __ster__(self):
+  def __str__(self):
     return f"{self.get_meal_display()} on {self.date}"
 
   class Meta:
--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 from .forms import FeedingForm
-# Create your views here.
 
 def home(request):
   return render(request, 'home.html')
@@ -18,12 +17,15 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
+  feeding_form = FeedingForm()
   return render(request, 'finches/detail.html', {
-    'finch': finch
+    'finch': finch,
+    'feeding_form': feeding_form
   })
 
 def add_feeding(request, finch_id):
   form = FeedingForm(request.POST)
+  print(form)
   if form.is_valid():
     new_feeding = form.save(commit=False)
     new_feeding.finch_id = finch_id
